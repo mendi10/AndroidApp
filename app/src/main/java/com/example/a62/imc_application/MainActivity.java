229dc91a1f0a2792c0e28d5becd5a5ceb7d68549
mendi10 <yesmendi@gmail.com>
2016-11-05 13:04:00 -0500

    app indice masa corporal -- clase 1

--- app/src/main/java/com/example/a62/imc_application/MainActivity.java
+++ app/src/main/java/com/example/a62/imc_application/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.a62.imc_application;
 
+import android.graphics.Color;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private TextView textViewresultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,36 +27,55 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        textViewresultado = (TextView) findViewById(R.id.textViewIMC);
+        Log.d(TAG,"Entra aca");
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+    public void calcularIMC(View view) {
+
+        Log.d(TAG,"Entra a calcularIMC con el click");
+
+        EditText editTextAltura = (EditText) findViewById(R.id.editTextAltura);
+        EditText editTextPeso = (EditText) findViewById(R.id.editTextPeso);
+
+        String altura = editTextAltura.getText().toString();
+        String peso = editTextPeso.getText().toString();
+
+            if(altura.isEmpty() & peso.isEmpty()) return;
+            if(altura.matches("\\d+") && peso.matches("\\d+")){
+                float kg = Integer.valueOf(peso);
+                float cm = Integer.valueOf(altura);
+                calcular(cm,kg);
             }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void calcular(float cm, float kg) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int color = 0;
+        cm = (cm/100);
+        float bmi = kg / (cm *cm);
+        String resultado;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            if(bmi < 16){
+                resultado = "TU BMI ES: " + bmi + " (Severamente bajo)";
+                color = Color.RED;
+            }else if(bmi < 18.5){
+                resultado = "TU BMI: ES" + bmi + " (Bajo)";
+                color = Color.YELLOW;
+            }else if(bmi < 25){
+                resultado = "TU BMI: ES" + bmi + " (Normal)";
+                color = Color.GREEN;
+            }else if(bmi < 30){
+                resultado = "TU BMI: ES" + bmi + " (sobre peso)";
+                color = Color.YELLOW;
+            }else {
+                resultado = "TU BMI: ES" + bmi + " (Obeso)";
+                color = Color.RED;
         }
+        Log.i(TAG,resultado);
 
-        return super.onOptionsItemSelected(item);
+        textViewresultado.setText(resultado);
+        textViewresultado.setTextColor(color);
     }
 }
